c8abb913ee69d5ebe15b988e94b9b39cba6a3e51
thinkaboutai <cuesky@gmail.com>
2022-07-11 10:01:46 +0800

    初始化2

---
+++ src/main/com/think/Demo.java
@@ -0,0 +1,4 @@
+package com.think;
+
+public interface Demo {
+}
--- src/main/com/think/Main.java
+++ src/main/com/think/Main.java
@@ -4,6 +4,7 @@ public class Main {
     // Hello world
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(1);
     }
 
 }
