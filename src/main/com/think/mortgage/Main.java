2083efa2eb755cca950b2f1954a8582d5a4fe0ab
thinkaboutai <cuesky@gmail.com>
2022-07-11 15:30:20 +0800

    新增对输入项的校验

--- src/main/com/think/mortgage/Main.java
+++ src/main/com/think/mortgage/Main.java
@@ -14,6 +14,13 @@ public class Main {
      *     <li>n：贷款时长（年）</li>
      * </ol>
      *
+     * <p>相关的输入的校验</p>
+     * <ol>
+     *      <li>贷款金额：1,000 ～ 1,000,000</li>
+     *      <li>贷款年利率：1 ～ 5</li>
+     *      <li>贷款时长：1 ～ 30</li>
+     * </ol>
+     *
      * @param args 控制台参数
      */
     public static void main(String[] args) {
@@ -23,18 +30,40 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         // 输入贷款金额
-        System.out.print("Principal：");
-        int principal = scanner.nextInt();
+
+        int principal;
+
+        while (true) {
+            System.out.print("Principal：");
+            principal = scanner.nextInt();
+            // 贷款金额在1000 - 1000000之间
+            if (principal >= 1_000 && principal <= 1_000_000) break; // 跳出循环体，进入下一步输入环节
+
+            System.out.println("Principal must be between 1000 and 1000000.");
+        }
 
         // 输入贷款金额
-        System.out.print("Years：");
-        int years = scanner.nextInt();
+        int years;
+        while (true) {
+            System.out.print("Years：");
+            years = scanner.nextInt();
+            if (years >= 1 && years <= 30) break; // 跳出循环体，进入下一步输入环节
+
+            System.out.println("Years must be between 1 and 30.");
+        }
         int numOfMonths = years * MONTHS_IN_YEAR;
 
+
         // 输入贷款年利率
-        System.out.print("Interest：");
-        float interest = scanner.nextFloat();
-        float rate = interest/ PERCENT / MONTHS_IN_YEAR;
+        float interest;
+        while (true) {
+            System.out.print("Interest：");
+            interest = scanner.nextFloat();
+            if (interest >= 1 && interest <= 5) break; // 跳出循环体，进入下一步输入环节
+
+            System.out.println("Interest must be between 1 and 5.");
+        }
+        float rate = interest / PERCENT / MONTHS_IN_YEAR;
 
         // BigDecimal
 
